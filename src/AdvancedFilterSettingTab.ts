6dcbf5970c3bfdd78e830b42cfa6f5e4be59765b
vlwkaos <mgparkprint@gmail.com>
2025-05-27 15:42:15 +0900

    feat: intial workspace feature added

---
+++ src/AdvancedFilterSettingTab.ts
@@ -0,0 +1,76 @@
+import { App, debounce, Notice, PluginSettingTab, Setting } from "obsidian";
+import AdvancedFilterPlugin from "./main";
+
+export class AdvancedFilterSettingTab extends PluginSettingTab {
+	public plugin: AdvancedFilterPlugin;
+
+	constructor(app: App, plugin: AdvancedFilterPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	public display(): void {
+		const { containerEl } = this;
+		containerEl.empty();
+
+		const plugin = this.plugin.getWorkspacesPlugin();
+		if (!plugin) {
+			containerEl.createEl('div', { text: 'Core plugin [Workspaces] not found. Please ensure it is enabled.' });
+			return;
+		}
+
+		// Add warning about override
+		const warning = containerEl.createEl('div', { cls: 'workspace-exclude-warning' });
+		warning.setText('⚠️ This plugin will override the core "Files and links" excluded files setting based on the conditions below.');
+
+		const { workspaces } = plugin;
+
+		// List all workspaces and provide per-workspace exclude configuration
+		if (workspaces.length === 0) {
+			containerEl.createEl('div', { text: 'No workspaces found.' });
+			return;
+		}
+
+		const notify = debounce((workspace) => {
+			new Notice(`${this.plugin.name}: Excluded files for workspace "${workspace}" updated.`);
+		}, 1000, true)
+
+
+		Object.keys(workspaces).forEach((workspace: string) => {
+			const section = containerEl.createEl('div', { cls: 'workspace-section' });
+			section.createEl('h3', { text: `Workspace: ${workspace}` });
+
+			new Setting(section)
+				.setName('Excluded files')
+				.setDesc('This will override the core "Files and links" excluded files setting for this workspace.')
+				.addTextArea(textarea => {
+					const value = this.plugin.settings.workspaceExcludes?.[workspace]?.join('\n') || '';
+					textarea
+						.setPlaceholder('work\npersonal')
+						.setValue(value)
+						.onChange(async (val) => {
+							if (!this.plugin.settings.workspaceExcludes) this.plugin.settings.workspaceExcludes = {};
+							this.plugin.settings.workspaceExcludes[workspace] = val.split('\n').map(s => s.trim()).filter(Boolean);
+							await this.plugin.saveSettings();
+							notify(workspace)
+						});
+				});
+		});
+
+		const section = containerEl.createEl('div');
+		section.createEl('h3', { text: 'Status bar' });
+		new Setting(section)
+			.setName('Show active workspace in status bar')
+			.setDesc('This will show the active workspace in the status bar.')
+			.addToggle(toggle => {
+				toggle
+					.setValue(this.plugin.settings.showWorkspaceNameInStatusBar ?? false)
+					.onChange(async (value) => {
+						this.plugin.settings.showWorkspaceNameInStatusBar = value;
+						await this.plugin.saveSettings();
+						this.plugin.toggleStatusBarItem(value);
+					});
+			})
+
+	}
+}
---
+++ src/main.ts
@@ -0,0 +1,89 @@
+import { Notice, Plugin } from 'obsidian';
+import { WorkspacesPluginInstance } from 'obsidian-typings';
+import { AdvancedFilterSettingTab } from './AdvancedFilterSettingTab';
+
+interface AdvancedFilterSettings {
+	workspaceExcludes: Record<string, (string | RegExp)[]>;
+	showWorkspaceNameInStatusBar?: boolean;
+}
+
+const DEFAULT_SETTINGS: AdvancedFilterSettings = {
+	workspaceExcludes: {}
+}
+
+export default class AdvancedFilterPlugin extends Plugin {
+	public settings: AdvancedFilterSettings;
+
+
+	private __previousActiveWorkspace: string | null = null;
+
+	private __statusBarItemEl: HTMLElement;
+
+	public async onload() {
+		await this.loadSettings();
+
+		this.__statusBarItemEl = this.addStatusBarItem();
+
+		this.addSettingTab(new AdvancedFilterSettingTab(this.app, this));
+		// There is no exposed event for workspace change, so we use layout-change as a workaround
+		this.registerEvent(this.app.workspace.on('layout-change', () => {
+			const currentActiveWorkspace = this.getWorkspacesPlugin()?.activeWorkspace;
+			const activeWorkspaceChanged = currentActiveWorkspace !== this.__previousActiveWorkspace;
+			if (!activeWorkspaceChanged) return;
+			this.setUserIgnoredFiltersByWorkspace(this.getWorkspacesPlugin()?.activeWorkspace);
+			this.__previousActiveWorkspace = currentActiveWorkspace ?? null;
+			this.__statusBarItemEl?.setText(this.getWorkspacesPlugin()?.activeWorkspace ?? 'Workspace not set');
+		}))
+
+		this.addCommand({
+			id: 'advanced-filter-disable',
+			name: `${this.name}: Clear Excluded Files temporarily.`,
+			callback: () => {
+				this.setUserIgnoredFiltersByWorkspace('')
+			}
+		})
+
+		this.toggleStatusBarItem(this.settings.showWorkspaceNameInStatusBar ?? false);
+	}
+	public get name() {
+		return 'Advanced Filter'
+	}
+
+	public toggleStatusBarItem(force?: boolean) {
+		if (!this.__statusBarItemEl) return;
+		const visible = this.__statusBarItemEl.checkVisibility()
+		this.__statusBarItemEl.style.display = force ?? !visible ? 'block' : 'none';
+	}
+
+	public onunload() {
+	}
+
+	public async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	public async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	public getWorkspacesPlugin(): WorkspacesPluginInstance | null {
+		const workspacesPlugin = this.app.internalPlugins.getEnabledPluginById('workspaces');
+		if (!workspacesPlugin) {
+			new Notice('Core plugin "Workspaces" not found. Please ensure it is enabled.');
+			return null;
+		}
+		return workspacesPlugin
+	}
+
+	public setUserIgnoredFiltersByWorkspace(workspaceName?: string) {
+		const plugin = this.getWorkspacesPlugin();
+		if (!plugin) return;
+		const workspace = workspaceName ?? plugin.activeWorkspace;
+		const excludes = this.settings.workspaceExcludes[workspace] ?? [];
+		// Excluded files setting
+		this.app.vault.setConfig('userIgnoreFilters', excludes)
+		new Notice(`${this.name}: Excluded files set for "${workspace}"`);
+	}
+
+}
+
